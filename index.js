92ff255b002224436c3d176ce912826d1431df6c
Gustavo Moneiro <dinizmonteirogustavo@gmail.com>
2020-02-24 20:32:10 -0300

    Add manifest.

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express()
 
 app.route('/').get((req, res) => res.status(200).json({ ok: true }))
 
-app.listen(3000, () => console.log('app listem port 3000'))
+app.listen(80, () => console.log('app listem port 80'))
